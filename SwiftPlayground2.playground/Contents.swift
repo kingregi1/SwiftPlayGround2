6b6822451a8b3ce8460b49d2905ffc23bc79f1e0
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-04 10:14:13 -1000

    Parts 1-4

--- SwiftPlayground2.playground/Contents.swift
+++ SwiftPlayground2.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var goodgrade = "I've been doing all of my work"
+
+if goodgrade == "Ive been doing all of my work"
+{
+print("I should be getting an A")
+}
+else
+    
+{
+    print("I need to work harder in this class")
+}
+var gradeA = true
+    
+if gradeA == true
+{
+print("I really like this class")
+}
+else
+{
+    print("I'm not sure about coding")
+}
